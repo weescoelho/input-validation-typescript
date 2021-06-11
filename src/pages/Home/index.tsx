20e26ad5e1a574e13dda3bb761273d0f18bd713a
weescoelho <wes.coelho18@gmail.com>
2021-06-10 21:19:33 -0300

    feat(home): add form to home component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,25 @@
+import React from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import styles from './Home.module.css';
+
 export function Home(): JSX.Element {
-  return <h1>Home page</h1>;
+  return (
+    <div className={styles.container}>
+      <form className={styles.form} noValidate autoComplete="off">
+        <TextField id="standard-basic" label="Username" />
+        <TextField id="standard-basic" label="E-mail" />
+        <TextField id="standard-basic" label="Password" />
+        <TextField id="standard-basic" label="Repeat password" />
+        <Button
+          style={{ marginTop: '20px' }}
+          variant="contained"
+          color="primary"
+        >
+          Enviar
+        </Button>
+      </form>
+    </div>
+  );
 }
