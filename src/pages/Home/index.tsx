2af358c41d0333960f2d749347255c2e26dfcb92
weescoelho <wes.coelho18@gmail.com>
2021-06-12 14:40:46 -0300

    feat(index): add type for HandleChange functions

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,10 +5,33 @@ import Button from '@material-ui/core/Button';
 import styles from './Home.module.css';
 
 export function Home(): JSX.Element {
+  const [username, setUserName] = React.useState<string>('');
+  const [email, setEmail] = React.useState<string>('');
+  const [password, setPassword] = React.useState<string>('');
+
+  type HandleChangeType = (event: React.FormEvent) => void;
+  const handleChangeInput: HandleChangeType = ({ target }) => {
+    console.log(target);
+  };
+
+  type HandleSubmitType = (event: React.FormEvent) => void;
+  const handleSubmit: HandleSubmitType = event => {
+    event.preventDefault();
+  };
+
   return (
     <div className={styles.container}>
-      <form className={styles.form} noValidate autoComplete="off">
-        <TextField id="standard-basic" label="Username" />
+      <form
+        onSubmit={handleSubmit}
+        className={styles.form}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          id="standard-basic"
+          label="Username"
+          onChange={handleChangeInput}
+        />
         <TextField id="standard-basic" label="E-mail" />
         <TextField id="standard-basic" label="Password" />
         <TextField id="standard-basic" label="Repeat password" />
@@ -16,6 +39,7 @@ export function Home(): JSX.Element {
           style={{ marginTop: '20px' }}
           variant="contained"
           color="primary"
+          type="submit"
         >
           Enviar
         </Button>
