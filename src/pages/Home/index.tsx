bb2cc9792ddbd310482517eb135d783ceeeeafd3
weescoelho <wes.coelho18@gmail.com>
2021-06-12 16:58:59 -0300

    refactor: remove unused states

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import useForm from 'hooks/useForm';
 import styles from './Home.module.css';
 
 export function Home(): JSX.Element {
-  const [username, setUserName] = React.useState<string>('');
-  const [email, setEmail] = React.useState<string>('');
-  const [password, setPassword] = React.useState<string>('');
+  const email = useForm('email');
+  const password = useForm('password');
 
   type HandleChangeType = (event: React.FormEvent) => void;
   const handleChangeInput: HandleChangeType = ({ target }) => {
@@ -17,6 +17,7 @@ export function Home(): JSX.Element {
   type HandleSubmitType = (event: React.FormEvent) => void;
   const handleSubmit: HandleSubmitType = event => {
     event.preventDefault();
+    console.log();
   };
 
   return (
@@ -32,9 +33,13 @@ export function Home(): JSX.Element {
           label="Username"
           onChange={handleChangeInput}
         />
-        <TextField id="standard-basic" label="E-mail" />
-        <TextField id="standard-basic" label="Password" />
-        <TextField id="standard-basic" label="Repeat password" />
+        <TextField id="email" label="E-mail" name="email" />
+        <TextField id="password" label="Password" name="password" />
+        <TextField
+          id="confirm_password"
+          label="Repeat password"
+          name="confirm_password"
+        />
         <Button
           style={{ marginTop: '20px' }}
           variant="contained"
